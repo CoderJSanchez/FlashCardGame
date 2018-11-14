e4433106b595f513d17c3eca27bfd4c7ada2fee5
Julio Sanchez <jsanchez191980802@gmail.com>
2018-11-13 18:47:42 -0600

    added redirects

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
 const express = require('express');
 const pug = require('pug');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
-const colors = [
-    'red',
-    'orange',
-    'yellow',
-    'blue',
-    'purple'
-];
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
+
 
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const name = req.cookies.username;
+    if(!name){
+        res.redirect('/hello')
+    }
+    res.render('index', { name });
 });
 
 app.get('/cards', (req, res) => {
-    res.render("card", {prompt: "Who is buried in Grants tomb", colors});
+    res.render("card", {prompt: "Who is buried in Grants tomb"});
+});
+
+app.get('/hello', (req, res) => {
+    const name = req.cookies.username;
+    if(name){
+        res.redirect('/')
+    }else{
+        res.render('hello');
+    }
+});
+
+app.post('/hello', (req, res) => {
+    res.cookie('username', req.body.username);
+    res.redirect('/');
+});
+
+app.post('/goodbye', (req, res) => {
+    res.clearCookie('username');
+    res.redirect('/hello');
 });
 
 app.listen(3000, () => {
